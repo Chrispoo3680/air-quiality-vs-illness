ae74fd6ee179437a4bfb08bc7017c4c5242fbfca
Chrispoo3680 <christobran@gmail.com>
2025-05-21 20:50:54 +0200

    Fixed location data fetching

--- src/common/tools.py
+++ src/common/tools.py
@@ -1,9 +1,12 @@
 import yaml
 from pathlib import Path
+import sys
 
 
 def load_config():
     # Read in the configuration file
-    with open("../../config.yaml") as p:  # Might need a better solution in the future
+    with open(
+        Path(sys.path[-1]) / "config.yaml"  # Might need a better solution in the future
+    ) as p:
         config = yaml.safe_load(p)
     return config
--- src/openaq_api.py
+++ src/openaq_api.py
@@ -1,28 +1,37 @@
 import requests
 import pycountry
 
-from common.tools import load_config
+from .common.tools import load_config
+from collections import defaultdict
 
 from typing import List, Dict
 
 
-def get_iso_country_codes(country_names: List[str]) -> List[int]:
-    country_dict: Dict[str, int] = {
-        country.name: country.numeric for country in list(pycountry.countries)
+def get_iso_country_codes(country_names: List[str]) -> List[str]:
+
+    country_dict: Dict[str, str] = {
+        country.name: country.alpha_2 for country in list(pycountry.countries)  # type: ignore
     }
 
-    filtered_iso_codes: List[int] = [country_dict[name] for name in country_names]
+    filtered_iso_codes: List[str] = [country_dict[name] for name in country_names]
 
     return filtered_iso_codes
 
 
-def get_location_ids(iso, limit=100):
+def get_location_ids(iso_codes: List[str], limit=100):
+
     config = load_config()
 
     endpoint = f"{config["openaq_api_url"]}locations"
+    headers = headers = {"X-API-Key": config["openaq_api_key"]}
 
-    params = {"countries_id": [79], "limit": 100}
+    locations = defaultdict(list)
 
-    response = requests.get(endpoint_locations, params=params, headers=headers)
+    for iso in iso_codes:
+        params = {"iso": iso, "limit": limit}
+        response = requests.get(endpoint, params=params, headers=headers)
 
-    locations = response.json()
+        for loc in response.json()["results"]:
+            locations[loc["country"]["code"]].append(loc)
+
+    return locations
